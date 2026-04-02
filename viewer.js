7878225950b7436fd2fd4ae6fde509a1423204e2
galecko-lgtm <galecko@gmail.com>
2026-04-02 08:47:01 -0700

    Update viewer.js

--- viewer.js
+++ viewer.js
@@ -1,4 +1,5 @@
-
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.160/build/three.module.js';
+import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.160/examples/jsm/loaders/GLTFLoader.js';
 
 window.addEventListener('DOMContentLoaded', () => {
 
